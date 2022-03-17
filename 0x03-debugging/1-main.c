f02faa0ffd8c42cd963652e6160d4b2413ba1e9f
Webamz <chriszxn99@gmail.com>
2022-03-17 12:26:30 -0700

    debugging

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - makes infinite loop
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	printf("infinite loop incoming :\n");
+	i = 0;
+	/*
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+	printf("infinite loop avoided! \\0/\n");
+	return (0);
+}
