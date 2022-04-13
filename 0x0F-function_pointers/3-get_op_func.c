333c5bd296e979b3095863521dbe3878b81dfa94
Webamz <chriszxn99@gmail.com>
2022-04-13 14:59:23 -0700

    corre

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,9 +18,9 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-	while (ops[i].op)
+	while (i < 5)
 	{
-		if (strcmp(ops[i].op, s) == 0)
+		if (*(ops[i]).op == *s && *(s + 1) == '\0')
 			return (ops[i].f);
 		i++;
 	}
