4eff96456a5b8904d45421da46e13dc511d7d48d
Webamz <chriszxn99@gmail.com>
2022-04-28 11:16:35 -0700

    bit manipulation

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * clear_bit - sets the value of a bit to 0 at index
+ * flip_bits - sets the value of a bit to 0 at index
  * @n: pointer of an unsigned long int
- * @index: index of the bit
+ * @m: index of the bit
  * Return: 1 for success, -1 for failure
  */
-int clear_bit(unsigned long int *n, unsigned int index)
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int m;
 
-	if (index > 63)
+	if (m > 63)
 		return (-1);
-	m = 1 << index;
+	m = 1 << m;
 	if (*n & m)
 		*n ^= m;
 	return (1);
